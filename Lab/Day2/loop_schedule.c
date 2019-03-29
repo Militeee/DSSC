91bfde095e3d49e509572d4d77c1b6f2df454ec6
Militeee <militesalvatore@gmail.com>
2019-03-29 15:05:33 +0100

    Exercise 2 done

--- Lab/Day2/loop_schedule.c
+++ Lab/Day2/loop_schedule.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <omp.h>
+ 
 
 void print_usage( int * a, int N, int nthreads ) {
 
@@ -18,8 +20,35 @@ void print_usage( int * a, int N, int nthreads ) {
 }
 
 int main( int argc, char * argv[] ) {
+  if(argc < 3){
+    printf("Not enough argument");
+    return -1;
+  }
+  int threads = atoi(argv[1]);
+  int chunks = atoi(argv[2]);
 
-  // TODO                                                                                                                                                                                                                                                                                
+  const int N = 250;
+  int a[N];
+  int b[N];
+  int nthreads = 0;
+  int i;
+  #pragma omp parallel  num_threads(threads)
+  {
+  int thread_id = omp_get_thread_num();
+  nthreads = omp_get_num_threads();
+  #pragma omp for schedule(static,chunks)  
+    for(i = 0; i < N; ++i) {
+      a[i] = thread_id;   
+    }
+  #pragma omp for schedule(dynamic,chunks)
+    for(i = 0; i < N; ++i) {
+      b[i] = thread_id;   
+    }
+  }
+  printf("Static schedule with ntreads = %d and chunk size = %d\n\n\n", nthreads,chunks );
+  print_usage(a,N,nthreads);
+  print_usage(b,N,nthreads);
 
   return 0;
+
 }
