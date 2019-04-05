0b7d0edb593c55616fa8d4cfc4556176bd6eb3b9
Salvatore Milite <smilite@login2.(none)>
2019-04-05 12:03:45 +0200

    final hoe2

--- Lab/Day2/pi_script.sh
+++ Lab/Day2/pi_script.sh
@@ -3,7 +3,10 @@ cd ./my_fork/Lab/Day2
 
 for i in 1 2 4 8 16 20; do
 	echo \#$i
+	printf "reduce\t"
 	./pi_reduce.x $i
+	printf "Atomic\t"
 	./pi_atom.x $i
+	printf "Critical1\t"
 	./pi_crit.x $i
 done;
