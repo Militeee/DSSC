0dc28bd84e5fa15e586f18cd28bc61c7ed2df80e
Salvatore Milite <smilite@login2.(none)>
2019-04-30 09:33:57 +0200

    Day4 done

---
+++ Lab/Day4/mpi_chain.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <mpi.h>
+#define N 100
+#define RIGHT(proc,nproc) (proc-1+nproc)%nproc
+#define LEFT(proc,nproc) (proc+1+nproc)%nproc
+
+int main( int argc, char * argv[] ) {
+  int rank = 0;
+  int npes = 1;
+  int* my = (int* )malloc(sizeof(int) * N);
+  int* sum = (int*) calloc(sizeof(int), N);
+  int* recv = (int* )malloc(sizeof(int) * N);
+  MPI_Init( &argc, &argv );
+  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+  MPI_Comm_size( MPI_COMM_WORLD, &npes );
+  for(int i = 0; i < N; i++){
+    my[i] = rank;
+  }
+  MPI_Request request;
+  for(int j = 0; j < npes-1; j++){
+    //sendin and exploting the time to do the summation
+    MPI_Isend(my, N, MPI_INT, LEFT(rank,npes), 101, MPI_COMM_WORLD, &request);
+    for(int i = 0; i < N; i++){
+      sum[i] += my[i];
+    }
+    MPI_Recv(recv, N, MPI_INT, RIGHT(rank,npes), 101, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Wait(&request, MPI_STATUS_IGNORE);
+    //change the pointers to propagate the sum
+    my = recv;
+  }
+  // doing the last sum
+  for(int i = 0; i < N; i++){
+    sum[i] += my[i];
+  }
+  printf("sum is %d\n", sum[0]);
+  MPI_Finalize();  
+  free(my); free(sum); free(recv);
+  return 0;
+}
---
+++ Lab/Day4/parallel_identity.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+#include <mpi.h>
+
+
+
+void print_matrix(double* M, int row, int col){
+  
+  for(int i = 0; i < row; i++){
+    for(int j = 0; j < col; j++)
+      printf("%f\t", M[i*col + j]);
+  printf("\n");  
+  }
+  
+}
+
+void fprint_matrix(double* M, int row, int col, FILE* fp){
+
+  for(int i = 0; i < row; i++){
+    for(int j = 0; j < col; j++)
+      fprintf(fp,"%.0f\t", M[i*col + j]);
+    fprintf(fp,"\n");  
+  }
+ }
+
+
+
+int main( int argc, char * argv[] ) {
+ 
+  int rank = 0;
+  int nproc = 1;
+  const int N = atoi(argv[1]);
+  MPI_Init( &argc, &argv );
+  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+  MPI_Comm_size( MPI_COMM_WORLD, &nproc );
+  int Nsize = N/nproc;
+  int rest = N%nproc;
+  if(rank < rest ) Nsize++;
+  int g_idx;
+  if(rank < rest ) g_idx = rank * Nsize;
+  else g_idx = (rank - rest) * Nsize + rest * (Nsize+1) ;
+  double* Buff;
+  if(rank == 0) Buff = (double*) malloc (sizeof(double) * N * Nsize);
+  int l_idx = 0;
+  double* Mat = (double*) malloc(sizeof(double) * N * Nsize);   
+  for(int i = g_idx; i < g_idx + Nsize; i++){
+    for(int j = 0; j < N; j++){
+      if(i == j) Mat[l_idx*N + j] = 1.;
+      else Mat[l_idx*N +j] = 0.;
+    }
+    l_idx++;
+  }
+  if(rank != 0)
+    MPI_Send(Mat, N*Nsize, MPI_DOUBLE, 0, 101, MPI_COMM_WORLD);
+  else{
+    FILE* f;
+    MPI_Request irreq; 
+    MPI_Status status;
+    int size;
+    int size_print;
+    double* tmp;
+    double* tmp2;
+    f = fopen("matrix_res.txt","w");
+    for(int i = 1; i < nproc+1; i++)
+    {
+      size_print = i == 1 ? Nsize : size;
+      tmp = i % 2 == 0 ? Mat : Buff;
+      tmp2 = i % 2 == 0 ? Buff : Mat; 
+      if(i < rest || rest == 0) size = Nsize;
+      else size = Nsize-1;
+      if(i != nproc)
+	MPI_Irecv(tmp, size * N, MPI_DOUBLE, i, 101, MPI_COMM_WORLD,&irreq);
+      if(N < 10) print_matrix(tmp2,size_print,N);
+      else
+	fprint_matrix(tmp2,size_print,N,f);
+      if(i != nproc)
+        MPI_Wait(&irreq,&status);
+    }
+    if(N > 10) fclose(f);
+}
+  free(Mat); 
+  if(rank == 0) free(Buff);
+  MPI_Finalize();
+    
+  return 0;
+}
