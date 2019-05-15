567d250593b54c416886e30b120694e74ccd28e0
Salvatore Milite <smilite@login2.(none)>
2019-05-15 15:18:55 +0200

    New transpose

---
+++ Lab/Day2/README.md
@@ -0,0 +1,5 @@
+#Day2
+
+Use make to compile, the executable have the source file name with .x, ./loop.x is the program that controls the thread execution, it takes two arguments
+
+./loop.x [number of threads] [chunk size]
--- Lab/Day5/cuda_script.sh
+++ Lab/Day5/cuda_script.sh
@@ -4,9 +4,9 @@ module load cudatoolkit/10.0
 
 
 for i in `seq 10`; do
-  ~/my_fork/Lab/Day5/Test/fast_transpose_64.x 8 8 >> ft_64.txt
-  ~/my_fork/Lab/Day5/Test/fast_transpose_128.x 8 16 >> ft_128.txt
-  ~/my_fork/Lab/Day5/Test/fast_transpose_256.x 16 16 >> ft_256.txt
-  ~/my_fork/Lab/Day5/Test/fast_transpose_512.x 16 32 >> ft_512.txt
-  ~/my_fork/Lab/Day5/Test/fast_transpose_1024.x 32 32 >> ft_1024.txt
+  ~/my_fork/Lab/Day5/Test/fast_transpose2_64.x >> ft2_64.txt
+  ~/my_fork/Lab/Day5/Test/fast_transpose2_128.x >> ft2_128.txt
+  ~/my_fork/Lab/Day5/Test/fast_transpose2_256.x >> ft2_256.txt
+  ~/my_fork/Lab/Day5/Test/fast_transpose2_512.x >> ft2_512.txt
+  ~/my_fork/Lab/Day5/Test/fast_transpose2_1024.x >> ft2_1024.txt
 done
